7fc809e8e0ab18f6a5aedfd5183cef427b16e8ae
unknown <ivaltekgbu@gmail.com>
2022-09-22 20:01:56 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenTK;
+
+namespace GameEngine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
